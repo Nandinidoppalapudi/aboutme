4c00ae7cd41f97e22ed9695dc4bf6a50ee7d4643
Nandinidoppalapudi <46694414+Nandinidoppalapudi@users.noreply.github.com>
2019-02-22 17:09:30 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 QUnit.test('Testing Area function with positive number sets of inputs', function (assert) {
  
-    assert.equal(myFunction(2,4),8,'Tested with small positive number 5');
+    assert.equal(myFunction(2,4),8,'Tested with small positive number ');
     assert.equal(myFunction(2,0),0,'Tested with zero number ');
-    assert.equal(myFunction(2,8),16,'Tested with large positive number 555');
+    assert.equal(myFunction(2,8),16,'Tested with large positive number');
  });
  
  QUnit.test('Testing Area function with negitive number sets of inputs', function (assert) {
  
-    assert.equal(myFunction(-2,6),-12,'Tested with small -ve number -5');
-    assert.equal(myFunction(-2,0),0,'Tested with small -ve number -1');
-    assert.equal(myFunction(-2,-8),16,'Tested with large -ve number -555');
+    assert.equal(myFunction(-2,6),-12,'Tested with small -ve number ');
+    assert.equal(myFunction(-2,0),0,'Tested with small -ve number ');
+    assert.equal(myFunction(-2,-8),16,'Tested with large -ve number ');
  });
  
  QUnit.test('Testing Area function with floating point number sets of inputs', function (assert) {
  
-    assert.equal(myFunction(2,5.6),11.2,'Tested with small floating point number 5.67');
-    assert.equal(myFunction(2,1.0),2.0,'Tested with small floating point number 1.0');
-    assert.equal(myFunction(2,5.2),10.4,'Tested with large floting number 555.555');
+    assert.equal(myFunction(2,5.6),11.2,'Tested with small floating point number ');
+    assert.equal(myFunction(2,1.0),2.0,'Tested with small floating point number ');
+    assert.equal(myFunction(2,5.2),10.4,'Tested with large floting number');
  });
  
 QUnit.test('Testing DOM Manipulation', function (assert) {
