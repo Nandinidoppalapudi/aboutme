5ac3a28a6217c36f5064814bc567ca708986ac1d
Nandinidoppalapudi <s534860@nwmissouri.edu>
2019-02-22 17:04:36 -0600

    update

---
+++ area.js
@@ -0,0 +1,10 @@
+
+length=0;
+breadth=0;
+
+function myFunction(length, breadth) {
+   
+        const area = length * breadth;
+        document.getElementById('result').innerHTML = area;
+        return area;
+    }
