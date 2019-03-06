a99f5970bad47715234645e81fa8a4cfa17cf8d3
Nandinidoppalapudi <s534860@nwmissouri.edu>
2019-03-05 20:15:12 -0600

    added ajax and local storage

--- area.js
+++ area.js
@@ -8,3 +8,17 @@ function myFunction(length, breadth) {
         document.getElementById('result').innerHTML = area;
         return area;
     }
+    
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("demo").innerHTML =
+          this.responseText;
+        }
+      };
+      xhttp.open("GET","nandini.txt", true);
+      
+      xhttp.send();
+    }
